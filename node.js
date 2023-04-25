7dba62e33f82bb2d8b55223de24468cc78f00f5e
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2023-04-25 19:25:28 +0530

    basic node

---
+++ node.js
@@ -0,0 +1,22 @@
+
+
+var http = require('http');
+var dt = require('./date.js');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  res.write.upperCase('Hello manage is hard ') + dt.myDate();
+
+  fs.readFile('empty.html' , 'utf-8', (err,data) => {
+       if(err){
+        res.write("404 found")
+      }
+      else{
+        res.write(data);
+      }
+  });
+  res.end();
+}).listen(1000);
+
+
