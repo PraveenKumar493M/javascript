3852b0d8aba7890f5d7f7ba070eb98fb97f20fb9
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2023-04-26 18:59:17 +0530

    Add files via upload

---
+++ nde.js
@@ -0,0 +1,13 @@
+var http = require ('http');
+
+http.createServer(function (req,res){
+res.writeHead(200,{'content-type':'text/html'});
+
+    res.write("<h1>this is form on node js.</h1>");
+    res.write('<form action="fileupload" method="post" enctype="mltipart/form-data"');
+    res.write('<input type="text" name="filetoupload"><br>');
+    res.write('<input type="file" name="filetoupload" required ><br>');
+    res.write('<input type="submit" name="filetoupload">');
+    res.write('</form>');
+    res.end();
+}).listen(8007);
