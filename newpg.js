f4867280edfee08b6ceeeae13671c8416e54fdd0
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2023-04-29 19:04:47 +0530

    sql pg

---
+++ newpg.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express();
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: '1q2w3e4r5t',
+  port: 5432,
+})
+
+app.get('/',(request, response) => {
+    pool.query('SELECT * FROM students', (error, results) => {
+      if (error) {
+        throw error
+      }
+      console.log(results.rows);
+      response.status(200).json(results.rows);
+    })
+  });
+
+  app.get('/user',(request,response)=>{
+    pool.query('SELECT * FROM public.kdata' , (error,results)=>{
+      if(error){
+        throw error;
+      }
+      response.status(200).json(results.rows);
+    })
+  })
+
+// const getUserById = (req,res)=>{
+//   const id = parseInt (req.params.Name)
+
+//   pool.query('SELECT * FROM students where Name = ram',[Name],(error,result)=>{
+//     if(error){
+//       throw(error);
+//     }
+//     res.status(200).json(result.rows);
+//   })
+// }
+
+  app.listen(2004);
