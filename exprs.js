bfc8680e6e8968bd32400b00d500909a635e52e6
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2023-04-29 19:04:08 +0530

    express js sample

---
+++ exprs.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 9000
+
+
+app.get('/', (req,res) => {
+    
+    res.send('Hello World')
+     }) 
+
+     app.put('/user', (req, res) => {
+        res.send('Got a PUT request at /user')
+      })
+
+app.post('/pos' , (req,res) =>{
+    res.send('got a post method')
+})
+
+    app.listen(port, () => {
+        console.log(`listen port ${port}`)
+    });
+
+   
+    
+
+
+
