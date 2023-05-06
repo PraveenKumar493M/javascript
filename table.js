f60d31f5cf6793158c1e148e15a48a98d8f2285c
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2023-05-06 19:04:28 +0530

    table model js

---
+++ table.js
@@ -0,0 +1,27 @@
+const { json } = require('body-parser');
+const express = require('express');
+const app = express();
+
+
+
+const Pool= require('pg').Pool;
+ const pool = new Pool({
+    user:'postgres',
+    host:'localhost',
+    database:'postgres',
+    password:'1q2w3e4r5t',
+    port:5432
+ })
+
+    app.get('/',(req,res)=>{
+  pool.query('SELECT * FROM kdata', (err,result) => {
+        console.log(result.rows);
+        res.status(200).json(result.rows);
+    });
+    })
+    
+   //  const getUserById = (request,response) = {
+   //    const {Name,email} = request.body
+   //    pool.query('INSERT INTO kdata ("ID", "Name", "Email", "Address", "Dept", "Year_joining", "Year_complete", "Currentlystudy_year")')
+   //  }
+    app.listen(3007);
