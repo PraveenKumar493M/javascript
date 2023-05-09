2b716e4ba55852c7952c2a84b39e6694bfbc6b3f
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2023-05-09 19:40:27 +0530

    Add files via upload

---
+++ indx.js
@@ -0,0 +1,22 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const db = require('./form.js');
+const port = 4000;
+
+app.use(bodyParser.json());
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+app.get('/',(request,response)=>{
+    response.json({info:'Node.js,express and postgres'})
+})
+app.get('/',db.getName);
+
+app.listen(port,()=>{
+    console.log(`port running ${port}.`)
+})
