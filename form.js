d616c554c799724aaf27ed089c134ae6d15dfbc2
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2023-05-10 19:16:19 +0530

    form

---
+++ form.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var app = express();
+
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user:'postgres',
+    host:'localhost',
+    database:'postgres',
+    password:'1q2w3e4r5t',
+    port:5432
+})
+
+const getName = (request,response) =>{
+    pool.query(`SELECT * FROM detail WHERE name`,(error,result) =>{
+         if(error){
+            throw error
+        }
+        response.json(result.rows);
+    });
+}
+
+ module.exports = {
+    getName
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// http.createServer((req,res)=>{
+//     res.writeHead(200,{'content-type':'text/html'})
+//     res.write('<form action = "/" method="post" enctype = "multipart/form-data>')
+//      res.write('<label for="name">Enter name: </label>')  
+//      res.write('<input type ="text" name="name" required><br>')
+//     res.write('<input type ="text" name="dept" required><br>')
+//     res.write('<input type ="text" name="degree" required><br>')
+//     res.write('<input type ="submit">')
+//     res.write('</form>')
+//     return res.end();
