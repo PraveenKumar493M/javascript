91dba4c1acca3cb3c9e5c661b0ebc00d9e1216da
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2023-05-12 19:15:45 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded(
+    {
+        extended: true
+        }
+))
+
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user:'postgres',
+    database:'postgres',
+    host:'localhost',
+    password:'1q2w3e4r5t',
+    port:5432
+})
+
+app.post('/show',(req,res)=>{
+    var inf = req.body.data;
+    let id = inf.id;
+    let name = inf.name;
+    let DOB = inf.DOB;
+    let phnumber = inf.phnumber;
+    let city = inf.city;
+    pool.query(`insert into medi (id,name,DOB,phnumber,city) values ('${id}','${name}','${DOB}','${phnumber}','${city}' )`);
+});
+
+app.get('/all',(req,res)=>{
+    let all = req.params.all;
+    pool.query('select * from medi',(error,result)=>{
+        res.send(result.rows);
+    });
+});
+
+app.get('/id/:data',(req,res)=>{
+    let data = req.params.data;
+    pool.query(`select * from medi where id=${data} `,(error,result)=>{
+        res.send(result.rows);
+    });
+});
+
+
+app.listen(5000);
+
+
+
+    // app.get('/name/:data',(req,res)=>{
+    //     let data = req.params.data;
+    //     pool.query(`select * from medi where name like '%${data}%' `,(error,result)=>{
+    //         res.send(result.rows);
+    //     });
+    // });
