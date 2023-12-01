e294adb80b08d54a8dd9dea02fd20c55266a1f7b
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2023-12-01 18:10:43 +0530

    Update prcts.js

--- prcts.js
+++ prcts.js
@@ -3,7 +3,6 @@ var uc = require('upper-case');
 
 http.createServer(function (req,res){
     res.writeHead(200,{'content-type':'text/html'});
-
     res.write(uc.upperCase('<h1>this is node js</h1>'+
     '<p>this tutorial</p>'+'<br><br>'+
     '<h6>there are field that grow in end that easy to acces.</h6>'));
