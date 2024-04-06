2577b539bfc72e3029600f8e5b0f515111649d75
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2024-04-06 10:30:00 +0530

    Update apitb database.js

--- apitb database.js
+++ apitb database.js
@@ -17,7 +17,6 @@ const pool = new Pool({
     password:'',
     port:
 });
-
 app.post('/show',(req,res)=>{
     var data = req.body;
     var id2 = parseInt(data.id);
@@ -29,7 +28,6 @@ app.post('/show',(req,res)=>{
         id, name, "DOB", phnumber, city)
         VALUES (${id2}, '${name2}', '${DOB2}', ${phnumber2}, '${city2}');`);
 });
-
 app.get('/all',(req,res)=>{
     let all = req.params.all;
     pool.query('select * from medi order by id',(error,result)=>{
