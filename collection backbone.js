7d7a25c0968e2a10253f0f954062ef06270d5c0d
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2024-06-06 19:44:50 +0530

    Update collection backbone.js

--- collection backbone.js
+++ collection backbone.js
@@ -9,7 +9,7 @@
     <script src = "https://cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.2/backbone-min.js"type = "text/javascript"></script> 
     <title>Backbonejs</title>
 </head> 
-<body> 
+<body>
     <div id="link">
     <a href="#/student1">student1</a>
     <a href="#/student2">student2</a>
