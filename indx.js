bac5e72b096fc0e146d201b716bf8b7dd255bd6b
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2024-06-14 19:13:01 +0530

    Update indx.js

--- indx.js
+++ indx.js
@@ -4,7 +4,6 @@ const app = express();
 const db = require('./form.js');
 const port = 4000;
 app.use(bodyParser.json());
-
 app.use(
     bodyParser.urlencoded({
         extended: true,
@@ -14,7 +13,6 @@ app.get('/',(request,response)=>{
     response.json({info:'Node.js,express and postgres'})
 })
 app.get('/',db.getName);
-
 app.listen(port,()=>{
     console.log(`port running ${port}.`)
 })
