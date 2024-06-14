1027077a5906d89c03fcb8825fdbefec1fd3e466
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2024-06-14 19:12:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,5 @@ app.get('/ab*cd', function(req, res) {
 var server = app.listen(8008, function () {
    var host = server.address().address
    var port = server.address().port
-   
    console.log("Example app listening at http://%s:%s", host, port)
 })
