b5625b0b60368f528b5341a2d69897f6ea188711
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2024-09-14 19:32:50 +0530

    Update node.js

--- node.js
+++ node.js
@@ -3,9 +3,7 @@ var dt = require('./date.js');
 var fs = require('fs');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-
   res.write.upperCase('Hello manage is hard ') + dt.myDate();
-
   fs.readFile('empty.html' , 'utf-8', (err,data) => {
        if(err){
         res.write("404 found")
