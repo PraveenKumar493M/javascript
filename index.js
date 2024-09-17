19128ecfb4c35eebad68709e8e6ccd441075597e
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2024-09-17 19:39:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,30 +4,22 @@ app.get('/g', function (req, res) {
    console.log("Got a GET request for the homepage");
    res.send('Hello GET');
 })
-
 app.post('/p', function (req, res) {
    console.log("Got a POST request for the homepage");
    res.send('Hello POST');
 })
-
-
 app.delete('/del_user', function (req, res) {
    console.log("Got a DELETE request for /del_user");
    res.send('Hello DELETE');
 })
-
-
 app.get('/list_user', function (req, res) {
    console.log("Got a GET request for /list_user");
    res.send('Page Listing');
 })
-
-
 app.get('/ab*cd', function(req, res) {   
    console.log("Got a GET request for /ab*cd");
    res.send('Page Pattern Match');
 })
-
 var server = app.listen(8008, function () {
    var host = server.address().address
    var port = server.address().port
