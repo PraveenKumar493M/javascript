4fbb3e145400d43191b612c9df19a8784f4cfafb
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2024-09-20 20:32:38 +0530

    Update nde.js

--- nde.js
+++ nde.js
@@ -1,7 +1,6 @@
 var http = require ('http');
 http.createServer(function (req,res){
 res.writeHead(200,{'content-type':'text/html'});
-
     res.write("<h1>this is form on node js.</h1>");
     res.write('<form action="fileupload" method="post" enctype="mltipart/form-data"');
     res.write('<input type="text" name="filetoupload"><br>');
