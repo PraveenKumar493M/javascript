d4e3dc666ee7dd34986b6945bf9bd4ee7bfc8cc3
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2024-10-01 19:34:55 +0530

    Update backboneview.js

--- backboneview.js
+++ backboneview.js
@@ -4,7 +4,6 @@ var ModeView1 = Backbone.Model.extend({
       name:"david"
     }
 });
-
 var DemoView1 = Backbone.View.extend({  
   initialize:function(){
       document.write("text can initialized.."+'</br>');
