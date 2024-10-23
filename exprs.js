c26d77598e09639677a8cca1bed3fb65c4ddab4d
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2024-10-23 18:36:43 +0530

    Update exprs.js

--- exprs.js
+++ exprs.js
@@ -4,14 +4,13 @@ const port = 9000
 app.get('/', (req,res) => {
     res.send('Hello World')
      }) 
-
-     app.put('/user', (req, res) => {
+app.put('/user', (req, res) => {
         res.send('Got a PUT request at /user')
       })
 app.post('/pos' , (req,res) =>{
     res.send('got a post method')
 })
-    app.listen(port, () => {
+app.listen(port, () => {
         console.log(`listen port ${port}`)
     });
 
