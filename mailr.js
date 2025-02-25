c6ec60f8f44f50ad07deca9084ea66fbfd6034af
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2025-02-25 20:01:33 +0530

    Update mailr.js

--- mailr.js
+++ mailr.js
@@ -6,12 +6,14 @@ var transporter = nodemailer.createTransport({
     pass: 'yourpassword'
   }
 });
+
 var mailOptions = {
   from: 'youremail@gmail.com',
   to: 'myfriend@yahoo.com',
   subject: 'Sending Email using Node.js',
   text: 'That was easy!'
 };
+
 transporter.sendMail(mailOptions, function(error, info){
   if (error) {
     console.log(error);
