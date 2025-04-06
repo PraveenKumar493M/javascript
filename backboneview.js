f10c8f18b5ea592c0d4b8b42b3cd454deee8732a
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2025-04-06 20:21:16 +0530

    Update backboneview.js

--- backboneview.js
+++ backboneview.js
@@ -13,6 +13,7 @@ var DemoView1 = Backbone.View.extend({
       document.write("my class name is ",this.className+'</br>');
   },
 });
+
 $(function(){
   var modeview1 = new ModeView1({site:"Tutorial point"})
   var demoview1= new DemoView1({
