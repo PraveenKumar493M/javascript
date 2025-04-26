68ea577cf89e692f30ab7fbeab3622ed24458835
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2025-04-26 17:22:50 +0530

    Update web.js

--- web.js
+++ web.js
@@ -3,9 +3,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded(
-    {
-        extended: true
-        }
+    { extended: true }
 ))
 
 const Pool = require('pg').Pool;
