ea10f131ff82dd4aa815c4d89fdf72798741231b
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2025-04-29 12:39:54 +0530

    Update prcts.js

--- prcts.js
+++ prcts.js
@@ -1,6 +1,5 @@
 var http = require('http');
 var uc = require('upper-case');
-
 http.createServer(function (req,res){
     res.writeHead(200,{'content-type':'text/html'});
     res.write(uc.upperCase('<h1>this is node js</h1>'+
