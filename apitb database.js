6a8b97457aa810b9a1e75f4bbfd9226240d2b7cf
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2025-08-09 13:21:27 +0530

    Update apitb database.js

--- apitb database.js
+++ apitb database.js
@@ -9,7 +9,6 @@ app.use(express.urlencoded(
      extended: true
     }
 ))
-
 const Pool = require('pg').Pool;
 const pool = new Pool({
     user:'postgres',
@@ -62,3 +61,4 @@ app.post('/update',(req,res)=>{
     })
 })
 app.listen(5000);
+
