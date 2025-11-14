d2967c119635e9afb3fe823c85d87a3428b5febf
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2025-11-14 15:07:49 +0530

    Update backbone event.js

--- backbone event.js
+++ backbone event.js
@@ -6,7 +6,6 @@ var Blog= Backbone.Model.extend({
         Url:''
     }
 });
-
 // collection
 var Blogs=Backbone.Collection.extend({});
 // var blog1=new Blog({
@@ -81,6 +80,7 @@ var blogsview=new BlogsView();
  
         
     
+
 
 
 
