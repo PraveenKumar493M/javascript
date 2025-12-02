96e7b7f4419b63aeee62aae411a51c2b06d8636d
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2025-12-02 18:07:17 +0530

    Update backbone sync.js

--- backbone sync.js
+++ backbone sync.js
@@ -2,11 +2,11 @@ Backbone.sync = function(method, model) {
     alert(method + ": " + JSON.stringify(model));
     model.set('id', 22);
   };
-
   var book = new Backbone.Model({
     title: "player coordination",
     author: "fernando"
   });
   book.save();
   book.save({author: "alba"});
+
 
